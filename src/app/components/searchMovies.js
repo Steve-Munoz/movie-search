a694eef835e71b73d12fa085d51e050bdcc4e8c3
Steve Munoz <stevomunoz@yahoo.com>
2023-12-15 17:53:47 -0600

    displayed movie information

--- src/app/components/searchMovies.js
+++ src/app/components/searchMovies.js
@@ -6,7 +6,7 @@ const SearchMovies = () => {
 
     const [query, setQuery] = useState("")
     const [movies, setMovies] = useState([])
-    const searchMovies = async () =>{
+    const searchMovies = async (e) =>{
         e.preventDefault();
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
@@ -31,6 +31,25 @@ const SearchMovies = () => {
         onChange = {(e)=> setQuery(e.target.value)}
         />
         <button className = "button" type = "submit">Search</button>
+
+        <div className = "card-list">
+            {movies.filter(movie=> movie.poster_path).map(movie=>
+                (<div className = "card" key= {movie.id}>
+                    <img className = "card"
+                    src = {`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                    alt={movie.title + ' poster'}
+                    /> 
+                    <div className = "card--content">
+                    <h3 className = "card--title">{movie.title}</h3>
+                    <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                    <p><small>RATING: {movie.vote_average}</small></p>
+                    <p className = "card--desc">{movie.overview}</p>
+                    </div>
+                
+                </div>)
+                )}
+
+        </div>
 
       
     </form>
